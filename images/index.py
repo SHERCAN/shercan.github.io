3372b941838efcc5ee75d2fece9858a5ed43dd18
SHERCAN <sidereoyess@gmail.com>
2023-04-06 12:21:31 -0500

    first commit

---
+++ images/index.py
@@ -0,0 +1,12 @@
+class carro:
+    def __init__(self, marca, modelo, ano, cor, km):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.cor = cor
+        self.km = km
+carro1=carro("Ford","Fiesta",2018,"Azul",0)
+carro2=carro("Ford","Fiea",201,"Czul",0)
+print(carro1)
+print(carro2)
+del
