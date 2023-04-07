27c5285415e02cdf51211772f356dbd7c8a54061
SHERCAN <sidereoyess@gmail.com>
2023-04-07 10:26:54 -0500

    mod js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,13 @@
 changeLanguage = document.getElementById("langChange");
 textChange = document.getElementById("textChange");
 changeLanguage.addEventListener("click", function () {
-  textChange.textContent = changeLanguage.checked ? "ES" : "EN";
-  //   if (changeLanguage.checked) {
-  //     textChange.console.log("clicked");
-  //   } else {
-  //     console.log("not clicked");
-  //   }
   let ruta = window.location.href;
   if (ruta.includes("en")) {
     ruta = ruta.replace("/en/", "/");
+    textChange.textContent = "ES";
   } else {
     ruta = ruta.replace("/in", "/en/in");
+    textChange.textContent = "EN";
   }
   console.log(ruta);
   window.location.href = ruta;
